e9d4d82c200bc78b89a640be5de328502b15ad6b
Reven <k3477a@gmail.com>
2025-11-11 15:16:53 +0800

    v1.3.2
    - 版本号

--- BilibiliPurify.js
+++ BilibiliPurify.js
@@ -2,7 +2,7 @@
 // @name         Bilibili Purify
 // @name:zh-CN   Bilibili纯粹化
 // @namespace    https://github.com/RevenLiu
-// @version      1.3.1
+// @version      1.3.2
 // @description  一个用于Bilibili平台的篡改猴脚本。以一种直接的方式抵抗商业化平台对人类大脑的利用。包含重定向首页、隐藏广告、隐藏推荐视频、评论区反成瘾/情绪控制锁等功能，削弱平台/媒体对你心理的操控，恢复你对自己注意力和思考的主导权。
 // @author       RevenLiu
 // @license      MIT
