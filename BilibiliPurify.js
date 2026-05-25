15ec1ef16c2d6a3dbb66cd0ced7e02afd0b5046c
Reven <k3477a@gmail.com>
2026-05-26 00:29:20 +0800

    v2.0.7
    - 补充隐藏项

--- BilibiliPurify.js
+++ BilibiliPurify.js
@@ -2,7 +2,7 @@
 // @name         Bilibili Purify
 // @name:zh-CN   Bilibili纯粹化
 // @namespace    https://github.com/RevenLiu
-// @version      2.0.6
+// @version      2.0.7
 // @description  一个用于Bilibili平台的篡改猴脚本。以一种直接的方式抵抗商业化平台对人类大脑的利用。包含重定向首页、隐藏广告、隐藏推荐视频、隐藏评论区等功能，削弱平台/媒体对你心理的操控，恢复你对自己注意力和思考的主导权。
 // @author       RevenLiu
 // @license      MIT
@@ -169,6 +169,10 @@
         'div.activity-gather-entry.activity-entry',
         //直播页主播昵称下点赞数
         'div.heat-index-scroll-wrapper.heat-index-scroll',
+        //直播页chat底部一起玩广告卡
+        'div.play-card-box',
+        //直播页播放效果评分弹窗
+        'div.research-container',
 
         //动态页视频播发量弹幕量显示
         'div.bili-dyn-card-video__stat__item',
